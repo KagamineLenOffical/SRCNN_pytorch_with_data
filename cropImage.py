81d835bf9a248ed2fdffdf0929d3460946c5cf5a
yanjiqiang <24320172203229@stu.xmu.edu.cn>
2021-03-13 20:49:43 +0800

    test version

---
+++ cropImage.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+src='Data/'
+dst='CropData/'
+size=32
+cnt=0
+imgs=os.listdir(src)
+for opath in imgs:
+    path=src+opath
+    print(path)
+    img=Image.open(path)
+    sz=img.size
+    print(sz)
+
+    for i in range(sz[0]//size):
+        for j in range(sz[1]//size):
+            crop_box=(i*size,j*size,(i+1)*size,(j+1)*size)
+            #print(crop_box)
+            crop_im=img.crop(crop_box)
+            #print(crop_im)
+            #crop_im.show()
+            crop_im.save(dst+'{}.bmp'.format(cnt))
+            cnt=cnt+1
